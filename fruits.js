6c29bffab0d3b54d80c24b370efad963594068e5
Richard Afro <richyafro@hotmail.com>
2019-02-25 11:25:26 +0100

    List of fruits

---
+++ fruits.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    "name": "Berries",
+    "price": 23.54,
+    "image": "https://images.unsplash.com/photo-1488900128323-21503983a07e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+    "description": "Sweet popsicles to help with the heat"
+  },
+  {
+    "name": "Orange",
+    "price": 10.33,
+    "image": "https://images.unsplash.com/photo-1504185945330-7a3ca1380535?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=340&w=340&q=80",
+    "description": "Mouth watering burger. Who cares if it's healthy"
+  },
+  {
+    "name": "Lemons",
+    "price": 12.13,
+    "image": "https://images.unsplash.com/photo-1504382262782-5b4ece78642b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+    "description": "Sumptuous egg sandwich"
+  },
+  {
+    "name": "Bananas",
+    "price": 10.33,
+    "image": "https://images.unsplash.com/photo-1478369402113-1fd53f17e8b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+    "description": "A great tower of pancakes. Dig in!"
+  },
+  {
+    "name": "Apples",
+    "price": 10.33,
+    "image": "https://images.unsplash.com/photo-1505253304499-671c55fb57fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+    "description": "Great looking Waffle to start the day"
+  },
+  {
+    "name": "Sharifa",
+    "price": 10.33,
+    "image": "https://images.unsplash.com/photo-1470119693884-47d3a1d1f180?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80",
+    "description": "What's greater than 5 minutes with grilled corn"
+  }
+]
